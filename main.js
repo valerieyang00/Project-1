f6a41f6889db7a2efcdacbb3d34069fdde076dcb
valerieyang00 <valerieyang00@gmail.com>
2022-08-20 10:38:43 -0700

    rewriting to structure in game loop

--- main.js
+++ main.js
@@ -1,129 +1,118 @@
 // const for HTML elements
 
     const startBtn = document.querySelector("#startBtn")
-    const canvas = document.querySelector("canvas")
+    const resetBtn = document.querySelector("#reset")
+    // const canvas = document.querySelector("canvas")
 
 //context for canvas
-    const ctx = canvas.getContext("2d")
-    canvas.setAttribute('height', getComputedStyle(canvas)['height'])
-    canvas.setAttribute('width', getComputedStyle(canvas)['width'])
+    // const ctx = canvas.getContext("2d")
+    // canvas.setAttribute('height', getComputedStyle(canvas)['height'])
+    // canvas.setAttribute('width', getComputedStyle(canvas)['width'])
+    // canvas.addEventListener("click", (e) => {
+    //     console.log(e.offsetX, e.offsetY)
+    // })
 
-//images for canvas and game pieces
-    const background = new Image()
-    const treasureImage = new Image()
-    const kittyImage = new Image()
-    background.src = "./media/v882-kul-46.jpg"
-    treasureImage.src = "./media/My project-1 (1).png"
-    kittyImage.src = "./media/cat.png"
-
-    background.onload = () => {
-    ctx.drawImage(background, 0, 0, 1000, 850)
-    }
-
-    treasureImage.onload = () => {
-    ctx.drawImage(treasureImage, 330, 305, 340, 240)
-    }
-
- 
-
-//functions to render and move kitty around
-
-class Crawler {
-    constructor(x, y, width, height, image) {
-    this.x = x
-    this.y = y
-    this.width = width
-    this.height = height
-    this.image = image
-    }
-    renderImages() {
-    this.image.onload = () => {    
-    ctx.drawImage(this.image, this.x, this.y, this.width, this.height)}}
-    }
-
-const kitty = new Crawler(200,200,100,100,kittyImage)
-
-
-function movementHandler(e) {
-    const speed = 10 // how many pixels kitty moves
-            switch (e.key) {
-            case('arrowUp'):
-                // move the hero up
-                kitty.y -= speed
-                if (kitty.y < 0) {
-                    kitty.y = 0
-                }
-                break
-            case('arrowDown'):
-                // move the kitty down
-                kitty.y += speed
-                if (kitty.y + kitty.height > canvas.height) {
-                    kitty.y = canvas.height - kitty.height
-                }
-                break
-            case('arrowLeft'):
-                // move the kitty left
-                kitty.x -= speed
-                if (kitty.x < 0) {
-                    kitty.x = 0
-                }
-                break
-            case('arrowRight'):
-                // move the kitty right
-                kitty.x += speed
-                if (kitty.x + kitty.width > canvas.width) {
-                    kitty.x = canvas.width - kitty.width
-                }
-                break
-            case('b'):
-                console.log("cat hisses, ants slow down")
-                break
-        }
-    }   
-
-document.addEventListener("keydown", movementHandler)
-
-// ant bases set up
-class staticObj {
-    constructor(x, y, width, height, color) {
-        this.x = x
-        this.y = y
-        this.width = width
-        this.height = height
-        this.color = color
-    }
-    render() {
-        ctx.fillStyle = this.color
-        ctx.fillRect(this.x, this.y, this.width, this.height)
+// const background = new Image()
+// background.src = "./media/v882-kul-46.jpg"
+// background.onload = () => {
+//     ctx.drawImage(background, 0, 0, 1000, 850)
+//     }
+let gameArea = {
+    canvas : document.querySelector("canvas"),
+    start : function () {
+        this.canvas.width = 1000;
+        this.canvas.height = 850;
+        this.context = this.canvas.getContext("2d");
+        canvas.setAttribute('height', getComputedStyle(canvas)['height']);
+        canvas.setAttribute('width', getComputedStyle(canvas)['width']);
+        this.interval = setInterval(updateGameArea, 60);
+    },
+    clear : function() {
+        this.context.clearRect(0,0,this.canvas.width, this.canvas.height);
     }
 }
 
-const baseTop = new staticObj(450, 99, 100, 10, "blue")
-const baseBtm = new staticObj(450, 744, 100, 10, "blue")
-const baseLeft = new staticObj(114, 375, 10, 100, "blue")
-const baseRight = new staticObj(881, 375, 10, 100, "blue")
+let background;
+let treasure;
+let kitty;
+let baseTop;
+let baseBtm;
+let baseLeft;
+let baseRight;
 
+function startGame () {
+    background = new component(0, 0, 1000, 850, "./media/v882-kul-46.jpg", "image")
+    treasure = new component(330, 305, 340, 240, "./media/My project-1 (1).png", "image")
+    kitty = new component (200, 200, 100, 100, "./media/cat.png", "image" )
+    baseTop = new component(450, 99, 100, 10, "blue")
+    baseBtm = new component(450, 744, 100, 10, "blue")
+    baseLeft = new component(114, 375, 10, 100, "blue")
+    baseRight = new component(881, 375, 10, 100, "blue")
+    gameArea.start();
 
-//game loops 
-window.requestAnimationFrame(gameLoop)
+}
 
-function gameLoop() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
+class component {
+    constructor (x, y, width, height, color, type) {
+    this.type = type;
+    if (type == "image") {
+        this.image = new Image();
+        this.image.src = color;
+    }    
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;
+    this.color = color;
+    this.render = () => {
+        const ctx = gameArea.context;
+        if (this.type == "image") {
+        window.onload = () => {
+            ctx.drawImage(this.image, this.x, this.y, this.width, this.height)};
+        } else {
+            ctx.fillStyle = this.color;
+            ctx.fillRect(this.x, this.y, this.width, this.height)}
+            }
+    this.newPos = () => {
+        this.x += this.speedX;
+        this.y += this.speedY;
+    }}}
+
+function updateGameArea() {
+    gameArea.clear();
     baseTop.render();
     baseBtm.render();
     baseLeft.render();
     baseRight.render();
-    kitty.renderKitty();
-
-
-
-window.requestAnimationFrame(gameLoop)
+    background.render();
+    treasure.render();
+    kitty.newPos();
+    kitty.render();
 }
 
+function moveUp() {
+    kitty.speedY -= 10;
+}
 
+function moveDown() {
+    kitty.speedY += 10;
+}
 
+function moveLeft() {
+    kitty.speedX -= 10;
+}
 
-// canvas.addEventListener("click", (e) => {
-//     console.log(e.offsetX, e.offsetY)
-// })
+function moveRight() {
+    kitty.speedX += 10;
+}
 
+function stopMove() {
+    kitty.speedX = 0;
+    kitty.speedY = 0;
+}
+
+//event listeners
+
+startBtn.addEventListener("click", startGame)
